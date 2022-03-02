10adbcdee7bb3c0d7dcfaf6b15632cec6c0ece21
heitorhenriques <heiitorheenrique@gmail.com>
2022-03-02 18:31:36 -0300

    update e delete

--- main.py
+++ main.py
@@ -8,15 +8,11 @@ if __name__ == '__main__':
 
     #Create.create_cliente('74.708.966/0001-14',fake.name())
     #Create.create_tipo_produto('Panela')
-    Create.create_produto('Cadeira de vidro',88.3,15)
+    #Create.create_produto('Panela normal',88.3,1)
     #teste = Read.read_produto()
     #print(type(teste[1]))
     #print(teste[1])
-<<<<<<< HEAD
     Update.update_produto(2,50)
-=======
-    #Delete.delete_tipo_produto()
->>>>>>> eddf4c272f05679c0eb40afe015c07a7f16fb568
 
 
     
